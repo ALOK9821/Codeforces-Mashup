f7863016b1aba5e32863154218f4852fe3c173fa
Alok Yadav <alokyadav9821238248@gmail.com>
2022-01-07 18:54:10 +0530

    material ui added

---
+++ src/Username/Userid.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Userid.scss"
+const Userid = () => {
+    return (
+        <>
+           <h1>Enter user name</h1>
+           <input type="text" placeholder='tourist' />
+        </>
+    )
+}
+
+export default Userid
