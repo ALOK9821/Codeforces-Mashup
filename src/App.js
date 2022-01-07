a14d2398475323bd41af43fbab865c1283eca81c
Alok Yadav <alokyadav9821238248@gmail.com>
2022-01-07 13:32:15 +0530

    heading completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Head from "./Header/Header"
 function App() {
   return (
     <div className="App">
-     <h1>Codeforces Problems generator</h1>
-     <h5>Get Unsolved Problems to generate Mashups/(Virtual contest) on Codeforces </h5>
+    <Head/>
     </div>
   );
 }
