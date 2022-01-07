dbf794e4bcf4afb81eb8d3d025c492955d8ebe94
Vishal Yadav <vy34365@gmail.com>
2022-01-07 19:32:18 +0530

    Added Buttons

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+// import Button from "./MyComponents/Buttons/Button";
+import Home from './MyComponents/Home';
 
 // Got it! (Vishal Yadav Here...)
+// https://codepen.io/chrisota/pen/bNdRaM
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+
+    <>
+      <Home />
+      
+    </>
   );
 }
 
